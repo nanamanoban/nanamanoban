92da9a3b5f837874e78ad75fd55d0f734261faa5
nanamanoban <100934886+nanamanoban@users.noreply.github.com>
2022-03-04 18:22:59 +0700

    Add files via upload

---
+++ nomer 2.py
@@ -0,0 +1,13 @@
+x = int(input("lebokno sak pengemu ben oleh angka genap: "))
+count = 0
+print("nyoh")
+while count <= x:
+    product = 0 + count
+    print(product)
+    count = count + 2
+print("iki angka genapmu bos")
+if x == product:
+    print("%d termasuk genap" %x)
+else:
+    print("%d  ganjil" %x)
+    
